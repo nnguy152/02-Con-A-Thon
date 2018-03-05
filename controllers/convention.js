b66043de2f727d94bfbb238e49f3ef9d142ce556
Nhi Nguyen <nnguy152@gmail.com>
2018-03-05 18:23:39 -0500

    make form for users to add in conventions, add comments for work todo later

--- controllers/convention.js
+++ controllers/convention.js
@@ -25,9 +25,8 @@ router.get('/', (req, res) => {
 // get convention by id from home page
 // "save" maybe push this convention into an array?
 // display this convention array on profile page
-// display this convention on profile page
+// PROFILE page
 router.get('/index', (req, res) => {
-  
   res.render('conventions/index')
 })
 
@@ -35,7 +34,6 @@ router.get('/index', (req, res) => {
 router.get('/signup', (req, res) => {
   res.render('signup', {message: req.flash('signupMessage')})
 })
-
 router.post('/signup', (req, res) => {
   var signupStrategy = passport.authenticate('local-signup', {
     successRedirect: '/',
@@ -47,7 +45,6 @@ router.post('/signup', (req, res) => {
 router.get('/login', (req, res) => {
   res.render('login', {message: req.flash('loginMessage')})
 })
-
 router.post('/login', (req, res) => {
   var loginProperty = passport.authenticate('local-login', {
     successRedirect: '/',
@@ -56,34 +53,43 @@ router.post('/login', (req, res) => {
   })
   return loginProperty(req, res)
 })
-
 router.get('/logout', (req, res) => {
   req.logout()
   res.redirect('/')
 })
 
+// NEW page. For users to add their own conventions
 router.get('/new', (req, res) => {
   res.render('conventions/new')
+})
+// for users to add their own convention in
+// after figure 'faving cons,' have it pushed to faveList array
+// to populate on profile
+router.post('/', (req, res) => {
+  Con.create({
+    name: req.body.name,
+    date: req.body.date,
+    location: req.body.location,
+    url: req.body.url,
+    genre: req.body.genre
+  })
+  .then(() => res.redirect('conventions/index'))
 })
 
 // Shows more details about each convention
 // "save" convention to display on profile page
 // add "save" button -when clicked, pushes convention into array
 // then in profile page, displays array^ router up there^^^
+var faveList = []
 router.get('/show/:id', (req, res) => {
-  // var favesList = []
   Con.findOne({_id: req.params.id})
-  // .then(con => {
-  //   var fave = document.querySelector('.fave')
-  //   fave.addEventListener('click', (con) => {
-  //     favesList.push(this.id)
-  //   })
-  // })
   .then(con => {
     res.render('conventions/show', con)
   })
 })
 
+// to edit but no function yet
+// should only be able to edit conventions that users self input
 router.get('/edit/:id', (req, res) => {
   Con.findOne({_id: req.params.id}).then(con => {
     res.render('conventions/edit', con)
