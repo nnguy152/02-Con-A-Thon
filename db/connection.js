5a59af4790f7390ebeda56c20a15abd6199468d7
Nhi Nguyen <nnguy152@gmail.com>
2018-03-06 20:00:47 -0500

    test

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const conConnect = require('mongoose')
 // const userConnect = require('mongoose')
 
-if (process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV === 'production') {
   conConnect.connect(process.env.MLAB_URL)
 } else {
   conConnect.connect('mongodb://localhost/conventions')
